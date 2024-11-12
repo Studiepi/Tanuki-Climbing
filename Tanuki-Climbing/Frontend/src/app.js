17065f1bed74f6627098667ca230a2e50bc70419
Studiepi <88stu88@gmail.com>
2024-11-12 20:34:59 +0200

    setup done

--- Tanuki-Climbing/Frontend/src/app.js
+++ Tanuki-Climbing/Frontend/src/app.js
@@ -1,17 +1,25 @@
-// src/app.js
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProductList from "./components/ProductList";
-import Cart from "./components/Cart";
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <div className="container">
+        <h1>Tanuki Climbing Shop</h1>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
+
--- Tanuki-Climbing/Frontend/src/firebase.js
+++ Tanuki-Climbing/Frontend/src/firebase.js
@@ -1,8 +1,7 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
+
